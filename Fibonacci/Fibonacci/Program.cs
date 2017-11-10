a9d9ab2b5f6dba2047a20c6f72120aa2c2fe3a0a
Gregory Dukes <kcils360@gmail.com>
2017-11-09 17:16:13 -0800

    added user instructions

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -6,6 +6,7 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a NUMBER to find it's position in the Fibonacci Sequence");
             int x = Convert.ToInt32(Console.ReadLine());
             Fibonacci go = new Fibonacci();
             go.FibSequence(0, 1, x);
