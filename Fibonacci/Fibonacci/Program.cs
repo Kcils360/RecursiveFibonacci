d7da814a03bc6ef0779ac13cfea730180cad58eb
Gregory Dukes <kcils360@gmail.com>
2017-11-09 17:10:10 -0800

    from sudo code, completed method

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Fibonacci go = new Fibonacci();
+            go.FibSequence(0, 1, x);
+
+            Console.ReadKey();
+        }
+    }
+}
